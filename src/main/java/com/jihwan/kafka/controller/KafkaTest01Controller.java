78d33c4ab783e5bbad4b7ba0205be1839d20d2e9
zeroTwo <wl5407@gmail.com>
2023-07-09 14:30:30 +0900

    feat: kafka 공식예제01

---
+++ src/main/java/com/jihwan/kafka/controller/KafkaTest01Controller.java
@@ -0,0 +1,66 @@
+package com.jihwan.kafka.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("kafka01")
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaTest01Controller {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final NewTopic myTopic1;
+    private final NewTopic myTopic2;
+
+
+    @GetMapping("/publish/mytopic1")
+    public String publishSpringTopic1() {
+
+        String message = "publish message to my_topic_1 " + UUID.randomUUID();
+
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(myTopic1.name(), message);
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.info("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+            }
+        });
+        return "done";
+    }
+
+    @GetMapping("/publish/mytopic2")
+    public String publish() {
+
+        String message = "publish message to my_topic_2 " + UUID.randomUUID();
+
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(myTopic2.name(), message);
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.info("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+            }
+        });
+        return "done";
+    }
+
+}
